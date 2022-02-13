e0a9e9723ecdec9d46e3738f07b3ff0d81bb87c0
Finn-Jones1 <47406390+finnjones@users.noreply.github.com>
2022-02-13 23:10:20 +1100

    Nothing

---
+++ old/voting/daml.js/38e6274601b21d7202bb995bc5ec147decda5a01b68d57dda422425038772af7/lib/index.js
@@ -0,0 +1,8 @@
+"use strict";
+/* eslint-disable-next-line no-unused-vars */
+function __export(m) {
+/* eslint-disable-next-line no-prototype-builtins */
+    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
+}
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.packageId = '38e6274601b21d7202bb995bc5ec147decda5a01b68d57dda422425038772af7';
---
+++ old/voting/daml.js/voting-0.0.1/lib/index.d.ts
@@ -0,0 +1,7 @@
+import * as User from './User';
+export { User } ;
+import * as Voting from './Voting';
+export { Voting } ;
+import * as VotingExample from './VotingExample';
+export { VotingExample } ;
+export declare const packageId = '8d553aff487fd71f85c9c6698dcf96555185da2beded887460a7962bd8cfab73';
