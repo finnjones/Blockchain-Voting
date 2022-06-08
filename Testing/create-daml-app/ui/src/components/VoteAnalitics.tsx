523eecb89437dbd0c776131f05650003a6a64405
finnjones <47406390+finnjones@users.noreply.github.com>
2022-06-08 20:08:23 +1000

    Working on charts

--- Testing/create-daml-app/ui/src/components/VoteAnalitics.tsx
+++ Testing/create-daml-app/ui/src/components/VoteAnalitics.tsx
@@ -27,94 +27,95 @@ import {
   useStreamQueries,
 } from "@daml/react";
 import {
-    Chart,
-    PieSeries,
-    Title
-  } from '@devexpress/dx-react-chart-material-ui';
-
+  Chart,
+  PieSeries,
+  Title,
+} from "@devexpress/dx-react-chart-material-ui";
+import { PieChart } from "react-minimal-pie-chart";
 
 const VoteAnalitics: React.FC = () => {
-    const username = useParty();
-    const ledger = useLedger();
-  
-    const myUserResult = useStreamFetchByKeys(User.User, () => [username], [
-      username,
-    ]);
-    const assets = useStreamQueries(Voting.Voting);
-    const myUser = myUserResult.contracts[0]?.payload;
-    const [radioStatus, setRadioStatus] = useState("");
-    const data = [
-        { argument:'Monday', value:10 },
-        { argument:'Tuesday', value:40 },
-        { argument:'Wednesday', value:10 },
-        { argument:'Thursday', value:20 },
-        { argument:'Friday', value:20 },
-      ];
+  const username = useParty();
+  const ledger = useLedger();
 
-    const map = assets.contracts[0]?.payload?.votes.reduce((acc, e) => acc.set(e, (acc.get(e) || 0) + 1), new Map());
-    console.log(map)
-    const buttonHandler = async () => {
-      if (assets.contracts[0]?.payload.voted.includes(username)) {
-        alert("You have already voted");
-      } else {
-        await ledger
-          .exerciseByKey(
-            Voting.Voting.Vote,
-            assets.contracts[0]?.signatories[0],
-            { voter: username, vote: radioStatus }
-          )
-          .catch(console.error);
-      }
-      console.log(assets.contracts[0]?.payload.voted);
-    };
-    //   const handleRadioButton = (event: any) => {
-    //     console.log(event);
-    //   };
-    // for (const [key, value] of map) {
-    //     console.log(key, value)
-    // }
-    return (
-      <Container>
-        <Grid centered columns={2}>
-          <Grid.Row stretched>
-            <Grid.Column>
-              <Header
-                as="h1"
-                size="huge"
-                color="blue"
-                textAlign="center"
-                style={{ padding: "1ex 0em 0ex 0em" }}
-              >
-                {myUser ? `Welcome, ${myUser.username}!` : "Loading..."}
-              </Header>
-              <Segment>
-                <Header as="h2">
-                  <Ballot sx={{ fontSize: 45 }} color="primary" />
-                  <Header.Content>
-                    Vote
-                    <Header.Subheader>
-                      Read the vote description carefully and choose an option
-                    </Header.Subheader>
-                  </Header.Content>
-                </Header>
-                <Divider />
-                <Header as="h3">Vote Description</Header>
-                {assets.contracts[0]?.payload?.subject ?? "Loading..."}
-                <Divider />
-                <Paper>
-                    <Chart
-                    data={map}
-                    >
-                    <PieSeries valueField="key" argumentField="value" />
-                    <Title text="Studies per day"/>
-                    </Chart>
-                </Paper>
-              </Segment>
-            </Grid.Column>
-          </Grid.Row>
-        </Grid>
-      </Container>
-    );
+  const myUserResult = useStreamFetchByKeys(User.User, () => [username], [
+    username,
+  ]);
+  const assets = useStreamQueries(Voting.Voting);
+  const myUser = myUserResult.contracts[0]?.payload;
+  const [radioStatus, setRadioStatus] = useState("");
+  const data = [
+    { title: "One", value: 10, color: "#E38627" },
+    { title: "Two", value: 15, color: "#C13C37" },
+    { title: "Three", value: 40, color: "#6A2135" },
+  ];
+
+  const map = assets.contracts[0]?.payload?.votes.reduce(
+    (acc, e) => acc.set(e, (acc.get(e) || 0) + 1),
+    new Map()
+  );
+  console.log(map);
+  console.log(data);
+  const buttonHandler = async () => {
+    if (assets.contracts[0]?.payload.voted.includes(username)) {
+      alert("You have already voted");
+    } else {
+      await ledger
+        .exerciseByKey(
+          Voting.Voting.Vote,
+          assets.contracts[0]?.signatories[0],
+          { voter: username, vote: radioStatus }
+        )
+        .catch(console.error);
+    }
+    console.log(assets.contracts[0]?.payload.voted);
   };
-  
-  export default VoteAnalitics;
+  //   const handleRadioButton = (event: any) => {
+  //     console.log(event);
+  //   };
+  // for (const [key, value] of map) {
+  //     console.log(key, value)
+  // }
+  return (
+    <Container>
+      <Grid centered columns={2}>
+        <Grid.Row stretched>
+          <Grid.Column>
+            <Header
+              as="h1"
+              size="huge"
+              color="blue"
+              textAlign="center"
+              style={{ padding: "1ex 0em 0ex 0em" }}
+            >
+              {myUser ? `Welcome, ${myUser.username}!` : "Loading..."}
+            </Header>
+            <Segment>
+              <Header as="h2">
+                <Ballot sx={{ fontSize: 45 }} color="primary" />
+                <Header.Content>
+                  Vote
+                  <Header.Subheader>
+                    Read the vote description carefully and choose an option
+                  </Header.Subheader>
+                </Header.Content>
+              </Header>
+              <Divider />
+              <Header as="h3">Vote Description</Header>
+              {assets.contracts[0]?.payload?.subject ?? "Loading..."}
+              <Divider />
+              <Paper>
+                <Chart data={data}>
+                  <PieSeries valueField="key" argumentField="value" />
+                  <Title text="Studies per day" />
+                </Chart>
+              </Paper>
+              <PieChart data={data} />
+            </Segment>
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
+    </Container>
+  );
+};
+
+export default VoteAnalitics;
